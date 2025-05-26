b64a91a8088bc97dabc558a4cf23c31e21bae8a7
endersis <alexisgt7168@gmail.com>
2025-05-26 15:56:58 -0600

    second push

---
+++ src/content/theme.js
@@ -0,0 +1,17 @@
+
+export const COLORS = {
+    primaryBlack: '#121212',
+    primaryRed: '#E53935',
+    white: '#FFFFFF',
+    grey: '#CCCCCC',
+  };
+  
+  export const SIZES = {
+    logoWidth: 412,
+    logoHeight: 882.61,
+    padding: 34.39,
+  };
+  
+  export const FONTS = {
+    // ...
+  };
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, SafeAreaView,Pressable } from 'react-native'; 
+import { StatusBar } from 'expo-status-bar';
+import { COLORS, SIZES } from '../content/theme'; 
+
+const icon = require('../assets/dml-logo.png'); 
+
+const MainScreen = () => {
+    const handleImagePress = () => {
+        console.log('Imagen presionada! Debería navegar a la pantalla de Login.');
+      };
+    
+
+  return (
+    <><SafeAreaView style={styles.safeArea}>
+          <StatusBar style="light" backgroundColor={COLORS.primaryBlack} />
+          <Pressable onPress={handleImagePress}>
+          <View style={styles.container}>
+              <Image
+                  source={icon}
+                  style={styles.logo} />
+          </View>
+          </Pressable>
+
+      </SafeAreaView><Text style={styles.brandText}>DLM</Text></>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlack,
+  },
+  container: {
+    
+    alignItems: 'center', 
+    justifyContent: 'center',
+    padding: SIZES.padding, 
+    backgroundColor: COLORS.primaryBlack,
+  },
+  logo: {
+    width: SIZES.logoWidth,  
+    height: SIZES.logoHeight,  
+    marginBottom: SIZES.padding, 
+  },
+  brandText: {
+    position: 'absolute',
+    left: 0,
+    right: 0, 
+    bottom: 80, 
+                             
+    textAlign: 'center', 
+    color: COLORS.black,
+    fontSize: SIZES.h1, 
+    fontWeight: 'bold',          
+   
+  },
+});
+
+export default MainScreen;
